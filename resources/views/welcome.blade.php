6d869ca89a9f94ceaca4fa6c530ae0d8ab9b58e3
Alexander <naxelord@gmail.com>
2023-03-31 16:30:02 +0300

    Added main greeting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,18 @@
     </head>
     <body class="antialiased">
         <x-app-layout>
-            
+            <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 h-84 flex justify-center items-center text-center mt-20">
+                <div class="p-6 space-x-2">
+                    <h1 id="message" class="text-3xl font-sans">
+                        Unleash your potential and inspire others.<br>
+                        Join our community to share your daily accomplishments and conquer your goals!
+                    </h1>
+
+                    <div id="registration_btn" class="mt-7 flex justify-center">
+                        <a href="{{ route('register') }}"class="bg-transparent hover:bg-black text-black font-semibold hover:text-white py-2 px-4 border border-black hover:border-transparent rounded">Try it now</a>
+                    </div>
+                </div>
+            </div>
         </x-app-layout>
         <script>
             // document.getElementById("registration_btn").style.display = "none";
