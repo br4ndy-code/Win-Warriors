a1443787be803f5a4239d40c38464543d97240bc
Alexander <naxelord@gmail.com>
2023-03-31 16:33:06 +0300

    Added logo to the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,10 @@
 <x-guest-layout>
+    {{-- Logo --}}
+    <div class="shrink-0 flex items-center justify-center">
+        <a href="{{ route('welcome') }}">
+            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+        </a>
+    </div>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
     <form method="POST" action="{{ route('login') }}">
