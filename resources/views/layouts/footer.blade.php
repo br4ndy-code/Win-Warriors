b8793792aee75ba00a89fb707cdf9562ae873a78
Alexander <naxelord@gmail.com>
2023-05-14 10:48:34 +0300

    Fixed footer position on the edit page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-neutral-900 text-center text-white">
     <div class="p-4 text-center">
       2023:
-      <a class="text-whitehite" href="https://github.com/br4ndy-code">Alexander Anshchuk</a>
+      <a class="text-whitehite" href="https://github.com/br4ndy-code">Created by Alexander Anshchuk</a>
     </div>
 </footer>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 h-screen">
         <form method="POST" action="{{ route('posts.update', $post) }}">
             @csrf
             @method('patch')
